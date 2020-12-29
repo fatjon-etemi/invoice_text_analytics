f230032f41e27f54da70848af9be7d59ca1b14dc
BOMATEC\fatjon.etemi <fatjon.etemi@bomatec.ch>
2020-12-29 22:25:35 +0100

    update

--- api.py
+++ api.py
@@ -1,11 +1,69 @@
-import flask
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+import pickle
+from pdf2image import convert_from_path
+import pytesseract
+from gensim.utils import simple_preprocess
+import json
+import pyodbc
+import pandas as pd
+from main import preprocess
 
-app = flask.Flask(__name__)
+ALLOWED_EXTENSIONS = {'pdf'}
+config = json.load(open('config.json'))
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 app.config["DEBUG"] = True
+app.config["UPLOAD_FOLDER"] = './uploads'
 
-@app.route('/', methods=['GET'])
-def home():
-    return 'Hello World!'
+model = pickle.load(open('doc2vec.pkl', 'rb'))
+svm_model = pickle.load(open('svm.pkl', 'rb'))
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def get_supplier(id, fields = ['name']):
+    server = config['server']
+    database = config['database']
+    username = config['db_user']
+    password = config['db_password']
+    cnxn = pyodbc.connect(
+        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+    query = "SELECT {} FROM abasbi.dbo.Vendor$Vendor_1_1 WHERE ID = '{}' AND MANDANT_ID = 2"
+    query = query.format(','.join(fields), id)
+    df = pd.read_sql(query, cnxn)
+    return df.loc[0].to_json()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            images = convert_from_path(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_string = ''
+            for image in images:
+                file_string += pytesseract.image_to_string(image)
+            vector = model.infer_vector(preprocess(file_string))
+            predicted_id = svm_model.predict([vector]).item(0)
+            return get_supplier(predicted_id)
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -11,11 +11,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score
 import pickle
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from gensim.sklearn_api import D2VTransformer
 from gensim.utils import simple_preprocess
 import pytesseract
 import json
 import matplotlib.pyplot as plt
 import random
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import lime
+from lime.lime_text import LimeTextExplainer
+from sklearn.datasets import fetch_20newsgroups
 
 path = '//srvaw03/AF-Drive/BACH/Vendor/B'
 pattern = '^PI20_[0-9]{6}$'
@@ -23,6 +30,17 @@ pattern1 = '^VE20_[0-9]*$'
 data_fields = 'nummer,sumbrutto,sumnetto,lief'
 data_path = './data'
 config = json.load(open('config.json'))
+
+
+def preprocess(document):
+    tokens = nltk.word_tokenize(document)
+    stop_words = stopwords.words('english') + stopwords.words('german')
+    tokens = [x for x in tokens if not x in stop_words]
+    tokens = [x.lower() for x in tokens]
+    tokens = [x for x in tokens if x.isalpha()]
+    porter = PorterStemmer()
+    tokens = [porter.stem(x) for x in tokens]
+    return tokens
 
 
 def get_files():
@@ -83,50 +101,39 @@ def merge():
     df.to_csv('train.txt')
 
 
-def clustering():
-    train = pd.read_csv('train.txt')
-    documents = [TaggedDocument(doc, [i]) for i, doc in enumerate(train["text"])]
-    model = Doc2Vec(documents, vector_size=100, window=2, min_count=1, workers=4)
-    model.train(documents, total_examples=len(documents), epochs=10)
-    number_of_clusters = len(set(train["lief"].tolist()))
-    kmeans_model = KMeans(n_clusters=number_of_clusters, max_iter=100)
-    X = kmeans_model.fit(model.docvecs.vectors_docs)
-    labels = kmeans_model.labels_.tolist()
-    print(labels)
-    l = kmeans_model.fit_predict(model.docvecs.vectors_docs)
-    pca = PCA(n_components=2).fit(model.docvecs.vectors_docs)
-    datapoint = pca.transform(model.docvecs.vectors_docs)
-
-    color = ["#" + ''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-             for i in range(len(labels))]
-    plt.scatter(datapoint[:, 0], datapoint[:, 1], c=color)
-
-    centroids = kmeans_model.cluster_centers_
-    centroidpoint = pca.transform(centroids)
-    plt.scatter(centroidpoint[:, 0], centroidpoint[:, 1], marker='^', s=150, c='#000000')
-    plt.show()
-
-
 def classification():
     train = pd.read_csv('train.txt')
     X = train["text"]
     Y = train["lief"]
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.33, random_state=42)
-    documents = [TaggedDocument(simple_preprocess(doc), [i]) for i, doc in enumerate(X_train)]
+    documents = [TaggedDocument(preprocess(doc), [i]) for i, doc in enumerate(X_train)]
     model = Doc2Vec(documents, vector_size=100, window=2, min_count=1, workers=4)
     model.train(documents, total_examples=len(documents), epochs=10)
-    svm_model = SVC()
+    svm_model = SVC(probability=True)
     svm_model.fit(model.docvecs.vectors_docs, Y_train)
     pickle.dump(model, open('doc2vec.pkl', 'wb'))
     pickle.dump(svm_model, open('svm.pkl', 'wb'))
-    X_test_vectors = [model.infer_vector(simple_preprocess(x)) for x in X_test]
+    X_test_vectors = [model.infer_vector(preprocess(x)) for x in X_test]
     print(svm_model.score(X_test_vectors, Y_test))
     print(f1_score(Y_test, svm_model.predict(X_test_vectors), average='weighted'))
 
+
+def explain():
+    train = pd.read_csv('train.txt')
+    X = train["text"]
+    Y = train["lief"]
+    model = pickle.load(open('doc2vec.pkl', 'rb'))
+    svm_model = pickle.load(open('svm.pkl', 'rb'))
+    explainer = LimeTextExplainer(class_names=set(Y))
+    idx = 1
+    vector = model.infer_vector(preprocess(X[idx]))
+    exp = explainer.explain_instance(X[idx], svm_model.predict_proba, num_features=len(vector))
+    exp.show_in_notebook()
 
 if __name__ == '__main__':
     # get_files()
     # get_data()
     # ocr()
     # merge()
-    classification()
+    # explain()
+    explain()
